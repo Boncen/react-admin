30b4e75e065e7c6df213620919ecd67b50e7ca72
boncen <boncen@outlook.com>
2024-03-06 20:50:53 +0800

    fix: 解决国际化失效的问题

--- web/src/locales/i18n.ts
+++ web/src/locales/i18n.ts
@@ -1,8 +1,8 @@
-import i18n from 'i18next'
-import { initReactI18next } from 'react-i18next'
-import LanguageDetector from 'i18next-browser-languagedetector'
-import en from './lang/en_US'
-import zh from './lang/zh_CN'
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+import en from "./lang/en_US";
+import zh from "./lang/zh_CN";
 
 i18n
   .use(LanguageDetector)
@@ -12,15 +12,15 @@ i18n
       en_US: { translation: en },
       zh_CN: { translation: zh },
     },
-    fallbackLng: 'en',
+    fallbackLng: "en_US",
     debug: false,
     react: {
       useSuspense: true,
     },
     detection: {
-      order: ['querystring', 'navigator', 'localStorage'],
-      lookupQuerystring: 'lang',
+      order: ["querystring", "navigator", "localStorage"],
+      lookupQuerystring: "lang",
     },
-  })
+  });
 
-export default i18n
+export default i18n;
